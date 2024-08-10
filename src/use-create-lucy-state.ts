29f2527c4b79370b50358b85ea2e76c7caf9b484
Seva Zaikov <seva.zaikov@gmail.com>
2024-08-09 20:46:29 -0700

    add comparator support to LucyState methods

--- src/use-create-lucy-state.ts
+++ src/use-create-lucy-state.ts
@@ -1,122 +1,162 @@
-import { useState, createElement, useEffect, useRef } from "react";
+import React, { useState, createElement, useEffect, useRef } from "react";
 
 const noValueSymbol = Symbol("no value");
 
-export function useCreateLucyState<T>(initialValue: T) {
+export function useCreateLucyState<T>(
+  initialValue: T,
+  comparator?: (a: T, b: T) => boolean
+) {
   let value = initialValue;
   let subscriptions: Function[] = [];
+
+  function LucyValueComponent<F = T>({
+    selector = (value) => value as unknown as F,
+    children,
+  }: {
+    selector?: (value: T) => F;
+    children: (value: F) => React.ReactNode;
+  }) {
+    const [state, setState] = useState(() => selector(value));
+    useEffect(() => {
+      const subscription = (newValue: T) => {
+        setState(selector(newValue));
+      };
+      subscriptions.push(subscription);
+
+      return () => {
+        subscriptions = subscriptions.filter((cb) => cb !== subscription);
+      };
+    }, []);
+
+    return children(state);
+  }
+
+  function useTrackValue<F>(
+    selector: (value: T) => F | undefined,
+    cb: (value: F) => void | Function,
+    {
+      skipFirstCall,
+      comparator,
+    }: { skipFirstCall?: boolean; comparator?: (a: F, b: F) => boolean } = {}
+  ) {
+    const dataRef = useRef<{
+      lastValue: typeof noValueSymbol | F;
+      initialized: boolean;
+      comparator: undefined | ((a: F, b: F) => boolean);
+      unsubscribe: undefined | Function;
+    }>({
+      lastValue: noValueSymbol,
+      initialized: false,
+      comparator,
+      unsubscribe: undefined,
+    });
+
+    if (!dataRef.current.initialized) {
+      dataRef.current.initialized = true;
+
+      if (!skipFirstCall) {
+        const selectedValue = selector
+          ? selector(value)
+          : (value as unknown as F);
+        const unsubscribe = cb(selectedValue);
+        dataRef.current.lastValue = selectedValue;
+        dataRef.current.unsubscribe = unsubscribe || undefined;
+      }
+    }
+
+    useEffect(() => {
+      const subscription = (newValue: T) => {
+        const newSelectedValue = selector
+          ? selector(newValue)
+          : (newValue as unknown as F);
+        const lastSelectedValue = dataRef.current.lastValue;
+
+        if (
+          !comparator ||
+          lastSelectedValue === noValueSymbol ||
+          !comparator(lastSelectedValue, newSelectedValue)
+        ) {
+          const unsubscribe = cb(newSelectedValue);
+          dataRef.current.lastValue = newSelectedValue;
+          if (dataRef.current.unsubscribe) {
+            dataRef.current.unsubscribe();
+          }
+          dataRef.current.unsubscribe = unsubscribe || undefined;
+        }
+      };
+      subscriptions.push(subscription);
+
+      return () => {
+        subscriptions = subscriptions.filter((cb) => cb !== subscription);
+      };
+    }, []);
+
+    useEffect(() => {
+      return () => {
+        const unsubscribe = dataRef.current.unsubscribe;
+        if (unsubscribe) unsubscribe();
+      };
+    }, []);
+  }
+
   return {
     // read the current value; helpful to read the latest value in callbacks/event handlers
     getValue: () => value,
     setValue: (newValue: T) => {
-      // small optimization to avoid running subscriptions
-      // if the referential equality is preserved
-      if (value === newValue) return;
-      value = newValue;
+      if (comparator) {
+        // don't rerun subscriptions if comparator says they are the same
+        if (comparator(value, newValue)) return;
+        value = newValue;
+      } else {
+        // small optimization to avoid running subscriptions
+        // if the referential equality is preserved
+        if (value === newValue) return;
+        value = newValue;
+      }
 
       subscriptions.forEach((cb) => cb(newValue));
     },
-    renderValue: (cb) => {
-      return createElement(function LucyElement() {
-        const [state, setState] = useState(value);
-        useEffect(() => {
-          const subscription = (newValue: T) => {
-            setState(newValue);
-          };
-          subscriptions.push(subscription);
-
-          return () => {
-            subscriptions = subscriptions.filter((cb) => cb !== subscription);
-          };
-        }, []);
-
-        return cb(state);
+    Value: LucyValueComponent,
+    renderValue: (
+      cb: (value: T) => React.ReactNode,
+      // this is for meta props, like `key` or `ref`
+      props?: Record<string, any>
+    ) => {
+      return createElement(LucyValueComponent, {
+        children: cb,
+        ...props,
       });
     },
-    renderValueSelector<F>(selector: (value: T) => F, cb) {
-      return createElement(function LucyElement() {
-        const [state, setState] = useState(() => selector(value));
-        useEffect(() => {
-          const subscription = (newValue: T) => {
-            setState(selector(newValue));
-          };
-          subscriptions.push(subscription);
-
-          return () => {
-            subscriptions = subscriptions.filter((cb) => cb !== subscription);
-          };
-        }, []);
-
-        return cb(state);
+    renderValueSelector<F>(
+      selector: (value: T) => F,
+      cb: (value: F) => React.ReactNode,
+      // this is for meta props, like `key` or `ref`
+      props?: Record<string, any>
+    ) {
+      return createElement(LucyValueComponent, {
+        children: cb,
+        selector,
+        ...props,
       });
     },
     useTrackValue: (
-      cb,
-      { skipFirstCall }: { skipFirstCall?: boolean } = {}
+      cb: (value: T) => void | Function,
+      options: {
+        skipFirstCall?: boolean;
+        comparator?: (a: T, b: T) => boolean;
+      }
     ) => {
-      useEffect(() => {
-        const subscription = (newValue: T) => {
-          cb(newValue);
-        };
-        subscriptions.push(subscription);
-
-        if (!skipFirstCall) {
-          cb(value);
-        }
-
-        return () => {
-          subscriptions = subscriptions.filter((cb) => cb !== subscription);
-        };
-      }, []);
+      useTrackValue<T>(undefined, cb, options);
     },
     useTrackValueSelector<F>(
       selector: (value: T) => F,
-      cb,
-      {
-        skipFirstCall,
-        comparator,
-      }: { skipFirstCall?: boolean; comparator?: (a: F, b: F) => boolean } = {}
+      cb: (value: F) => void | Function,
+      options: {
+        skipFirstCall?: boolean;
+        comparator?: (a: F, b: F) => boolean;
+      } = {}
     ) {
-      const dataRef = useRef<{
-        lastValue: typeof noValueSymbol | F;
-        initialized: boolean;
-        comparator: undefined | ((a: F, b: F) => boolean);
-      }>({
-        lastValue: noValueSymbol,
-        initialized: false,
-        comparator,
-      });
-
-      if (!dataRef.current.initialized) {
-        dataRef.current.initialized = true;
-
-        if (!skipFirstCall) {
-          const selectedValue = selector(value);
-          cb(selectedValue);
-          dataRef.current.lastValue = selectedValue;
-        }
-      }
-
-      useEffect(() => {
-        const subscription = (newValue: T) => {
-          const newSelectedValue = selector(newValue);
-          const lastSelectedValue = dataRef.current.lastValue;
-
-          if (
-            !comparator ||
-            lastSelectedValue === noValueSymbol ||
-            !comparator(lastSelectedValue, newSelectedValue)
-          ) {
-            cb(newSelectedValue);
-            dataRef.current.lastValue = newSelectedValue;
-          }
-        };
-        subscriptions.push(subscription);
-
-        return () => {
-          subscriptions = subscriptions.filter((cb) => cb !== subscription);
-        };
-      }, []);
+      useTrackValue<F>(selector, cb, options);
     },
   };
 }

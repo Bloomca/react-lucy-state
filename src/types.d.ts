64390ad7c5cba0eb7d31ee1e10ef5e856d90e14b
Seva Zaikov <seva.zaikov@gmail.com>
2024-09-01 08:04:13 -0700

    leave state.Value as the only way to render data

--- src/types.d.ts
+++ src/types.d.ts
@@ -10,21 +10,6 @@ export type LucyState<T> = {
     selector?: (value: T) => F;
     children: (value: F) => React.ReactNode;
   }) => React.ReactNode;
-  renderValue: (
-    cb: (value: T) => React.ReactNode,
-    props?: Record<string, any>
-  ) => React.FunctionComponentElement<{
-    selector?: (value: T) => unknown;
-    children: (value: unknown) => React.ReactNode;
-  }>;
-  renderValueSelector<F>(
-    selector: (value: T) => F,
-    cb: (value: F) => React.ReactNode,
-    props?: Record<string, any>
-  ): React.FunctionComponentElement<{
-    selector?: (value: T) => unknown;
-    children: (value: unknown) => React.ReactNode;
-  }>;
   useTrackValue: (
     cb: (value: T) => void | Function,
     options: {

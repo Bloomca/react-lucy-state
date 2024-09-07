46be870b51ef21c2129f5ad50ea85b4c3d6cf647
Seva Zaikov <seva.zaikov@gmail.com>
2024-09-07 01:45:41 -0700

    refactor zustand hook to accept dependencies array

---
+++ src/library-connectors/zustand.test.tsx
@@ -0,0 +1,220 @@
+import { useEffect } from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { createStore } from "zustand/vanilla";
+
+import { useZustandHook } from "./zustand";
+import { useLucyState } from "../use-lucy-state";
+
+describe("useZustandHook", () => {
+  it("subscribes to zustand updates correctly", async () => {
+    const user = userEvent.setup();
+    let newValue = "updated value";
+    const zustandStore = createStore<{
+      value: string;
+      updateValue: () => void;
+    }>((set) => ({
+      value: "first value",
+      updateValue() {
+        set({ value: newValue });
+      },
+    }));
+
+    function Component() {
+      const zustand$ = useZustandHook({
+        store: zustandStore,
+        selector: (state) => state.value,
+      });
+
+      return (
+        <div>
+          <h2>
+            Current value is: <zustand$.Value />
+          </h2>
+          <button onClick={() => zustandStore.getState().updateValue()}>
+            Change value
+          </button>
+        </div>
+      );
+    }
+
+    render(<Component />);
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: first value"
+    );
+
+    await user.click(screen.getByRole("button", { name: "Change value" }));
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: updated value"
+    );
+
+    newValue = "another value";
+
+    await user.click(screen.getByRole("button", { name: "Change value" }));
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: another value"
+    );
+  });
+
+  it("does not re-render components if the value has not changed", async () => {
+    const spy = jest.fn();
+    const user = userEvent.setup();
+    let newValue = "updated value";
+    let newNum = 25;
+    const zustandStore = createStore<{
+      value: string;
+      num: number;
+      updateValue: () => void;
+      updateNum: () => void;
+    }>((set) => ({
+      value: "first value",
+      num: 10,
+      updateValue() {
+        set({ value: newValue });
+      },
+      updateNum() {
+        set({ num: newNum });
+      },
+    }));
+
+    function Content({ value }: { value: string }) {
+      useEffect(spy);
+
+      return <h2>Current value is: {value}</h2>;
+    }
+
+    function Component() {
+      const zustand$ = useZustandHook({
+        store: zustandStore,
+        selector: (state) => state.value,
+      });
+      const num$ = useZustandHook({
+        store: zustandStore,
+        selector: (state) => state.num,
+      });
+
+      return (
+        <div>
+          <zustand$.Value>
+            {(value) => <Content value={value} />}
+          </zustand$.Value>
+          <h3>
+            Current num is: <num$.Value />
+          </h3>
+          <button onClick={() => zustandStore.getState().updateValue()}>
+            Change value
+          </button>
+          <button onClick={() => zustandStore.getState().updateNum()}>
+            Change num
+          </button>
+        </div>
+      );
+    }
+
+    render(<Component />);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: first value"
+    );
+    expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
+      "Current num is: 10"
+    );
+
+    await user.click(screen.getByRole("button", { name: "Change num" }));
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: first value"
+    );
+    expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
+      "Current num is: 25"
+    );
+
+    await user.click(screen.getByRole("button", { name: "Change value" }));
+
+    expect(spy).toHaveBeenCalledTimes(2);
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: updated value"
+    );
+    expect(screen.getByRole("heading", { level: 3 })).toHaveTextContent(
+      "Current num is: 25"
+    );
+  });
+
+  it("re-renders when the dependency changes", async () => {
+    const user = userEvent.setup();
+    let newValue = {
+      first: "updated first value",
+      second: "updated second value",
+    };
+    let newSelection = "second";
+    const zustandStore = createStore<{
+      value: {
+        first: string;
+        second: string;
+      };
+      updateValue: () => void;
+    }>((set) => ({
+      value: {
+        first: "first value",
+        second: "second value",
+      },
+      updateValue() {
+        set({ value: newValue });
+      },
+    }));
+
+    function Component() {
+      const select$ = useLucyState("first");
+      const zustand$ = useZustandHook({
+        store: zustandStore,
+        selector: (state, [selection]) => state.value[selection],
+        dependencies$: [select$],
+      });
+
+      return (
+        <div>
+          <h2>
+            Current value is: <zustand$.Value />
+          </h2>
+          <button onClick={() => select$.setValue(newSelection)}>
+            Change selection
+          </button>
+          <button onClick={() => zustandStore.getState().updateValue()}>
+            Change value
+          </button>
+        </div>
+      );
+    }
+
+    render(<Component />);
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: first value"
+    );
+
+    await user.click(screen.getByRole("button", { name: "Change selection" }));
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: second value"
+    );
+
+    await user.click(screen.getByRole("button", { name: "Change value" }));
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: updated second value"
+    );
+
+    newSelection = "first";
+    await user.click(screen.getByRole("button", { name: "Change selection" }));
+
+    expect(screen.getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Current value is: updated first value"
+    );
+  });
+});
--- src/library-connectors/zustand.ts
+++ src/library-connectors/zustand.ts
@@ -1,22 +1,128 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useLucyState } from "../use-lucy-state";
 
 import type { UseBoundStore, StoreApi } from "zustand";
+import type { LucyState } from "../types";
 
-export function useZustandHook<T, F>(
-  store: UseBoundStore<StoreApi<T>> | StoreApi<T>,
-  selector: (state: T) => F
-) {
-  const state = useLucyState(selector(store.getState()));
+export function useZustandHook<T, F, A, B, C>({
+  store,
+  selector,
+  dependencies$,
+}:
+  | {
+      store: UseBoundStore<StoreApi<T>> | StoreApi<T>;
+      selector: (state: T, dependencies?: [A, B, C]) => F;
+      dependencies$?: [LucyState<A>];
+    }
+  | {
+      store: UseBoundStore<StoreApi<T>> | StoreApi<T>;
+      selector: (state: T, dependencies?: [A, B, C]) => F;
+      dependencies$?: [LucyState<A>, LucyState<B>];
+    }
+  | {
+      store: UseBoundStore<StoreApi<T>> | StoreApi<T>;
+      selector: (state: T, dependencies?: [A, B, C]) => F;
+      dependencies$?: [LucyState<A>, LucyState<B>, LucyState<C>];
+    }) {
+  const firstValue = useRef<F | null>(null);
+  if (firstValue.current === null) {
+    firstValue.current = selector(
+      store.getState(),
+      transformState$(dependencies$) as any
+    );
+  }
+  const state$ = useLucyState(firstValue.current);
+  const itemsRef = useRef(dependencies$);
+
+  const updateStateRef = useRef(null);
+  if (!updateStateRef.current) {
+    updateStateRef.current = (currentStoreState: T) => {
+      const newValue = selector(
+        currentStoreState,
+        transformState$(itemsRef.current) as any
+      );
+      state$.setValue(newValue);
+    };
+  }
+
+  const unsubscribeRef = useRef<null | (() => void)>(null);
+
+  // to ensure we run the updates function only one time
+  // in case this component re-renders
+  if (unsubscribeRef.current === null) {
+    unsubscribeRef.current = subscribeToUpdates(dependencies$, () => {
+      updateStateRef.current(store.getState());
+    });
+  }
 
   useEffect(() => {
     const unsbuscribe = store.subscribe((currentState) => {
-      const newValue = selector(currentState);
-      state.setValue(newValue);
+      updateStateRef.current(currentState);
     });
 
     return unsbuscribe;
-  }, [store, state]);
+  }, [store, state$]);
 
-  return state;
+  useEffect(() => {
+    if (!areArraysEqual(dependencies$, itemsRef.current)) {
+      itemsRef.current = dependencies$;
+      updateStateRef.current(store.getState());
+
+      if (unsubscribeRef.current) unsubscribeRef.current();
+
+      unsubscribeRef.current = subscribeToUpdates(dependencies$, () => {
+        updateStateRef.current(store.getState());
+      });
+    }
+  }, [dependencies$]);
+
+  useEffect(() => {
+    return () => {
+      if (unsubscribeRef.current) unsubscribeRef.current();
+    };
+  }, []);
+
+  return state$;
+}
+
+function areArraysEqual(array1: any[] | undefined, array2: any[] | undefined) {
+  // both are undefined
+  if (!array1 && !array2) return true;
+  // if one is undefined, but another is not, we need to recalculate
+  if (!array1 && array2) return false;
+  if (array1 && !array2) return false;
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+}
+
+function transformState$(items$: LucyState<any>[]) {
+  if (!items$) return [];
+  return items$.map((item$) => item$.getValue());
+}
+
+function subscribeToUpdates(
+  items$: LucyState<any>[] | undefined,
+  onChange: () => void
+) {
+  if (!items$) {
+    return undefined;
+  }
+
+  const unsubscribeFns = [];
+  items$.forEach((item$) => {
+    const unsubscribe = item$.trackValue(() => {
+      onChange();
+    });
+
+    unsubscribeFns.push(unsubscribe);
+  });
+
+  return () => {
+    unsubscribeFns.forEach((fn) => fn());
+  };
 }
